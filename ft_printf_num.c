94ddc4ae02893dec2cb78faa111977a80525761f
alvinyap510 <leaf.alvin@gmail.com>
2023-04-11 00:00:31 +0800

    fix: header

--- ft_printf_num.c
+++ ft_printf_num.c
@@ -10,6 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
 #include "libft/libft.h"
 
 int ft_nbr_digits(int n)
